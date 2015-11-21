d23e8d5688ca8191266d449fad8155f76305e86b
Derrick Ho <derrickho328@gmail.com>
2015-11-21 17:57:31 -0500

    test

--- MyPN/AppDelegate.swift
+++ MyPN/AppDelegate.swift
@@ -7,15 +7,115 @@
 //
 
 import UIKit
+import Parse
+
+protocol MyPNProtocol {
+    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData)
+    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject])
+    func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError)
+}
+
+enum MyPNStateMachine {
+    case NotificationsDisabledInIOSSettings
+    
+}
+
+public
+class MyPN {
+    static let sharedInstance: MyPN = MyPN()
+    
+    /**
+    - You get the generated deviceToken from application(_: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken: NSData)
+    - Device tokens will be generated whenever MyPN::registerForPn() is called as long as at least one of the following is enabled in iOS Settings: "Show in Notification Center", "Sounds", "Badge App Icon", "Show on Lock Screen"
+    - Device tokens will be generated when app goes to foreground as long as the app's iOS Notifications settings ("Show in Notification Center", "Sounds", "Badge App Icon", "Show on Lock Screen") go from an off state to an on state
+    */
+    static var deviceToken: NSData! // NEEDS to be SET before USE
+    
+    init() {
+    }
+    
+    static func deviceTokenAsString() -> String {
+        let devChar = MyPN.deviceToken.description.characters
+        return Array(devChar)[1..<(devChar.count - 1)].map({ String($0)}).reduce("", combine: { $0 + $1 })
+    }
+    
+    /**
+    This will cause application(_: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken: NSData) to be called.
+    */
+    static public func registerForPn() {
+        UIApplication.sharedApplication()
+            .registerUserNotificationSettings(
+                UIUserNotificationSettings(
+                    forTypes: [
+                        UIUserNotificationType.Badge,
+                        UIUserNotificationType.Sound,
+                        UIUserNotificationType.Alert
+                    ],
+                    categories: nil
+                )
+        )
+        UIApplication.sharedApplication().registerForRemoteNotifications()
+    }
+
+    /**
+    Unregistering does not seem to effect the ios notifications settings.  However, unregistering makes the device identifiers stop comming in.  Unregistering is like sealing the mailbox of your app.  THe mail man( notification center) can still come but won't be able to put anything in.  THEREFORE NOT UNREGISTER!
+    */
+    static public func unregisterForPn() {
+//        UIApplication.sharedApplication().unregisterForRemoteNotifications()
+    }
+    
+    /**
+    Shows Alerts that are currently enabled
+    */
+    static func enabledUserNotificationsAsArray() -> [String] {
+        let b = UIApplication.sharedApplication().currentUserNotificationSettings()
+        var arr: [String] = []
+        let bb = b!.types.rawValue
+        if bb & UIUserNotificationType.Alert.rawValue != 0 {
+            arr += ["Alert"]
+        }
+        if bb & UIUserNotificationType.Badge.rawValue != 0 {
+            arr += ["Badge"]
+        }
+        if bb & UIUserNotificationType.Sound.rawValue != 0 {
+            arr += ["Sound"]
+        }
+        return arr
+    }
+
+}
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, MyPNProtocol {
 
     var window: UIWindow?
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+//        Parse.setApplicationId("vY2qwKErWmbSVtLjVEHjbpOkOEu5wnUcHvD5wZBC",
+//            clientKey: "SEaPnsEVGmH3YJqLh8XHmwB4h0MpjRusvNA85NnM")
+//        
+//        // Register for Push Notitications
+//        if application.applicationState != UIApplicationState.Background {
+//            // Track an app open here if we launch with a push, unless
+//            // "content_available" was used to trigger a background push (introduced in iOS 7).
+//            // In that case, we skip tracking here to avoid double counting the app-open.
+//            
+//            let preBackgroundPush = !application.respondsToSelector("backgroundRefreshStatus")
+//            let oldPushHandlerOnly = !self.respondsToSelector("application:didReceiveRemoteNotification:fetchCompletionHandler:")
+//            var pushPayload = false
+//            if let options = launchOptions {
+//                pushPayload = options[UIApplicationLaunchOptionsRemoteNotificationKey] != nil
+//            }
+//            if (preBackgroundPush || oldPushHandlerOnly || pushPayload) {
+//                PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+//            }
+//        }
+        
+        MyPN.registerForPn()
+        
         return true
     }
 
@@ -41,6 +141,43 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    // MARK: - sdfasdfasd
+    
+    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
+        print(userInfo)
+        
+//        PFPush.handlePush(userInfo)
+//        if application.applicationState == UIApplicationState.Inactive {
+//            PFAnalytics.trackAppOpenedWithRemoteNotificationPayload(userInfo)
+//        }
+    }
+    
+    func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
+        print(error)
+//        if error.code == 3010 {
+//            print("Push notifications are not supported in the iOS Simulator.")
+//        } else {
+//            print("application:didFailToRegisterForRemoteNotificationsWithError: %@", error)
+//        }
+    }
+    
+    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
+        
+//        let installation = PFInstallation.currentInstallation()
+//        installation.setDeviceTokenFromData(deviceToken)
+//        installation.channels = ["global"]
+//        installation.saveInBackground()
+        
+        /////
+        
+        
+        MyPN.deviceToken = deviceToken
+        print(MyPN.deviceTokenAsString())
+//        print(deviceToken)
+//       let s = Array(deviceToken.description.characters)[1..<(deviceToken.description.characters.count - 1)].map({ String($0)}).reduce("", combine: { $0 + $1 })
+//        print(s)
+    }
+    
 
 }
 
--- MyPN/ViewController.swift
+++ MyPN/ViewController.swift
@@ -8,10 +8,96 @@
 
 import UIKit
 
+public var deviceToken:String = ""
+
+public func registerForPn() {
+    UIApplication.sharedApplication()
+        .registerUserNotificationSettings(
+            UIUserNotificationSettings(
+                forTypes: [
+                    UIUserNotificationType.Badge,
+                    UIUserNotificationType.Sound,
+                    UIUserNotificationType.Alert
+                ],
+                categories: nil
+            )
+    )
+    UIApplication.sharedApplication().registerForRemoteNotifications()
+    
+}
+
 class ViewController: UIViewController {
 
+    lazy var regPn: UIButton = {
+        let b = UIButton(frame: CGRect(x: 50, y: 50, width: 100, height: 100))
+        b.setTitle("regPn", forState: UIControlState.Normal)
+        b.addTarget(self, action: "pressedRegisterPN:", forControlEvents: UIControlEvents.TouchUpInside)
+        return b
+    }()
+    
+    lazy var unregPn: UIButton = {
+        let b = UIButton(frame: CGRect(x: 150, y: 50, width: 100, height: 100))
+        b.setTitle("unregPn", forState: UIControlState.Normal)
+        b.addTarget(self, action: "pressedUnregisterPN:", forControlEvents: UIControlEvents.TouchUpInside)
+        return b
+        }()
+    
+    lazy var isRegForRemoteLabel: UILabel = {
+       let l = UILabel(frame: CGRect(x: 50, y: 150, width: 100, height: 100))
+        l.textColor = UIColor.whiteColor()
+        return l
+    }()
+    
+    lazy var refreshIsRegButton: UIButton = {
+        let b = UIButton(frame: CGRect(x: 150, y: 150, width: 200, height: 100))
+        b.setTitle("refreshIsRegButton", forState: UIControlState.Normal)
+        b.addTarget(self, action: "pressedIsRegistered:", forControlEvents: UIControlEvents.TouchUpInside)
+        return b
+        }()
+    
+    lazy var deviceTokenLabel: UILabel = {
+        let l = UILabel(frame: CGRect(x: 50, y: 250, width: 100, height: 100))
+        l.textColor = UIColor.whiteColor()
+        return l
+        }()
+    
+    lazy var refreshDeviceToken: UIButton = {
+        let b = UIButton(frame: CGRect(x: 150, y: 250, width: 200, height: 100))
+        b.setTitle("refreshDeviceToken", forState: UIControlState.Normal)
+        b.addTarget(self, action: "pressedRefeshDeviceToken:", forControlEvents: UIControlEvents.TouchUpInside)
+        return b
+        }()
+    
+    override func loadView() {
+        self.view = UIView()
+        self.view.addSubview(regPn)
+        self.view.addSubview(unregPn)
+        self.view.addSubview(isRegForRemoteLabel)
+        self.view.addSubview(refreshIsRegButton)
+        self.view.addSubview(deviceTokenLabel)
+        self.view.addSubview(refreshDeviceToken)
+    }
+    
+    
+    func pressedRegisterPN(sender: AnyObject) {
+       MyPN.registerForPn()
+    }
+    
+    func pressedUnregisterPN(sender: AnyObject) {
+        MyPN.unregisterForPn()
+    }
+    
+    func pressedIsRegistered(sender: AnyObject) {
+        self.isRegForRemoteLabel.text = MyPN.enabledUserNotificationsAsArray().count > 0 ? "YES" : "NO"
+    }
+    
+    func pressedRefeshDeviceToken(sender: AnyObject) {
+        deviceTokenLabel.text = deviceToken
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
